56ea635ad8358ee4128f63c1f749e5b9e3d46f76
bernadette <aguirre.bernadette@outlook.com>
2022-03-26 20:27:23 -0600

    CompletedAssignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = 'BOB'
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'bob'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'sam'
+    return leastFavoriteCustomer;
+}
